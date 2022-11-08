b8e8bbad39c439eddc76dc42eb4c552515c0ce56
Darwin Lazzar <davont417@gmail.com>
2022-11-07 20:35:56 -0800

    Player Movement Updated
    basic functionality completed. Movement and animation, facing mouse.

--- The Legend Of Dave/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ The Legend Of Dave/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -23,15 +23,33 @@ public class PlayerMovement : MonoBehaviour
         //make diagonal speed not 1.4x faster
         movement = movement.normalized;
 
+        //movement animations
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        //making player face the direction of the mouse
+        faceMouse();
     }
 
     void FixedUpdate() 
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
+    }
+
+    void faceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        //if the mouse is to the left of the player it'll face left
+        if(mousePosition.x - transform.position.x >= 0){
+            rb.transform.localScale = new Vector3(1,1,1); 
+        //else it'll face right
+        }else{
+            rb.transform.localScale = new Vector3(-1,1,1);
+        }
     }
 }
 
