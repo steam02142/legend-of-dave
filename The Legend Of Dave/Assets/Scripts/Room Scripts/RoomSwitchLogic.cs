a98dd814f7098fbf9f034dcd204810fc2705f450
Turbu-lent <117727904+Turbu-lent@users.noreply.github.com>
2022-11-18 21:04:27 -0800

    Room Switch Update
    + Player can now switch rooms
    + Room exit Prefab Object
    + Object allows input of any scene name

--- The Legend Of Dave/Assets/Scripts/Room Scripts/RoomSwitchLogic.cs
+++ The Legend Of Dave/Assets/Scripts/Room Scripts/RoomSwitchLogic.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoomSwitchLogic : MonoBehaviour
 {
+    public string sceneToLoad;
+    
+    //When fully implemented the script won't need to be given a room to go to
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("objectedajdshflk");
+    if (other.tag == "Player") {
+        SceneManager.LoadScene(sceneToLoad);
+    }
     }
 }
