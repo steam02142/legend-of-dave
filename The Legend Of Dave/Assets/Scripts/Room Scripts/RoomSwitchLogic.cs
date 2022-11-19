734522575c272d3c619565a3710dd54024bfebee
Turbu-lent <117727904+Turbu-lent@users.noreply.github.com>
2022-11-18 21:21:27 -0800

    Room Exit Random Logic
    + Room Exit now goes to a random room

--- The Legend Of Dave/Assets/Scripts/Room Scripts/RoomSwitchLogic.cs
+++ The Legend Of Dave/Assets/Scripts/Room Scripts/RoomSwitchLogic.cs
@@ -6,11 +6,15 @@ using UnityEngine.SceneManagement;
 public class RoomSwitchLogic : MonoBehaviour
 {
     public string sceneToLoad;
+    private string s;
     
     //When fully implemented the script won't need to be given a room to go to
     private void OnTriggerEnter2D(Collider2D other)
     {
     if (other.tag == "Player") {
+        int randomNum = Random.Range(1, 2+1); //not actually for 3 rooms. idk why but you have to add 1 LMAO
+        s = randomNum.ToString();
+        sceneToLoad = "Room" + s;
         SceneManager.LoadScene(sceneToLoad);
     }
     }
